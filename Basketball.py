80711f0b1518bf5237869bc63fb6e90f02371516
Jose Pestana <165429503+PestanaJose@users.noreply.github.com>
2025-01-06 01:53:59 -0500

    Add files via upload

---
+++ Basketball.py
@@ -0,0 +1,14 @@
+import sqlite3 as sql
+import pandas as pd
+
+# Connect to the SQLite database
+conn = sql.connect('../input/basketball/basketball.sqlite')
+
+# Run a query and load it into a Pandas DataFrame
+df_games = pd.read_sql_query("SELECT * FROM game", conn)
+
+# Close the database connection
+conn.close()
+
+# Display the first few rows
+print(df_games.head())
